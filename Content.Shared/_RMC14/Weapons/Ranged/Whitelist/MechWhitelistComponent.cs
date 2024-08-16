b53556d17ad3dbad1e4aab25737a2da76d23350c
Magic0stick <77047717+Magic0stick@users.noreply.github.com>
2024-08-16 19:29:26 +0300

    мех Бегун МKI был добавлен
    мех Бегун МKI был добавлен

---
+++ Content.Shared/_RMC14/Weapons/Ranged/Whitelist/MechWhitelistComponent.cs
@@ -0,0 +1,8 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared._RMC14.Weapons.Ranged.Whitelist;
+
+[RegisterComponent, NetworkedComponent]
+[Access(typeof(CMGunSystem))]
+[SpecialistSkillComponent("Mech")]
+public sealed partial class MechWhitelistComponent : Component;
